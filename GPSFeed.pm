207afc9cedfdbdea91decbda3d0df812c56565e0
Ben <ben.ma.adams@gmail.com>
2013-09-21 17:11:41 -0500

    Rewrite to eliminate GPSd and read the serial device directly

--- GPSFeed.pm
+++ GPSFeed.pm
@@ -1,7 +1,7 @@
 #
 ####   GPSFeed   ####
 #
-# A class to simply working with GPSd
+# A class to simply pulling raw GPS NMEA data
 #
 
 #--------------------------------------------------#
@@ -17,13 +17,17 @@ package GPSFeed;
 use warnings;
 use strict;
 use Carp;
-use IO::Socket::INET;
-use JSON qw( decode_json );
 use Time::Local 'timelocal_nocheck';
+use FileHandle;
+use Data::Dumper;
 
 sub new {
 	my $package = shift @_;
 	my %init = @_;
+
+	unless (defined $init{devpath} && -r $init{devpath}) {
+		croak("The GPS device path was not specified, or the given path is not readable");
+	}
 
 	$init{debug} = undef unless (defined $init{debug} && $init{debug} > 0);
 
@@ -40,59 +44,36 @@ sub initialize {
 	my $self = shift;
 	my $debug = $self->{debug};
 
-	print "Connecting to GPSd..." if $debug;
-	$self->{gpsd} = IO::Socket::INET->new('localhost:2947')
-		or return [ undef, "Could not connect to GPSd on localhost:2947: $@" ];
-	print "[1;32mOK[0m\n" if $debug;
+	print "Opening GPS device file for reading...\n" if $debug;
+	$self->{devfeed} = FileHandle->new("< " . $self->{devpath});
 
-	print "Validating GPSd version..." if $debug;
-	my $greeting = $self->{gpsd}->getline();
-	my $json = decode_json($greeting);
-	if ($json) { print "[1;32m", $$json{release}, "[0m\n" if $debug; }
-	else       { return [ undef, "Made connection to GPSd, but did not receive greeting string" ]; } 
-
-	print "Activating GPS data feed..." if $debug;
-	$self->{gpsd}->print("?WATCH={\"enable\":true,\"json\":true}\n");
-	until ($$json{class} eq "DEVICES") {
-		my $rawtext = $self->{gpsd}->getline();
-		$json = decode_json($rawtext);
+	if (! defined $self->{devfeed}) {
+		return [ undef, "Failed to open GPS device path: $@" ];
 	}
 
-	if (scalar @{$$json{devices}} == 0) {
-		return [ undef, "There is no (functional) GPS device connected to the system" ];
-	}
-	else {
-		my $gpsdev = shift @{$$json{devices}};
-
-		unless (defined ($$gpsdev{activated})) {
-			return [ undef, "There is no GPS device connected to the system" ];
-		}
-
-		printf "[1;32m%s[0m\n", $$gpsdev{path} . " @ " . $$gpsdev{bps} . "bps" if $debug;
-	}
-
-	my $gpstimeout = 20;
-	print "Waiting for good data ($gpstimeout secs max)..." if $debug;
+	my $gpstimeout = 10;
+	print "Waiting for usable NMEA position sentence ($gpstimeout secs max)...\n" if $debug;
 	eval {
 		local $SIG{ALRM} = sub { die "alarm\n" };
 		alarm $gpstimeout;
-		until ($$json{mode} && $$json{mode} == 3) {
-			my $rawtext = $self->{gpsd}->getline();
-			$json = decode_json($rawtext);
+		my $grabline = "";
+		until ($grabline =~ /^\$GPRMC,.*?,A,/) {
+			$grabline = $self->{devfeed}->getline();
 		}
 		alarm 0;
+
+		my $parsedNMEA = $self->decodeNMEA($grabline);
+		$self->{lon} = $$parsedNMEA{lon};
+		$self->{lat} = $$parsedNMEA{lat};
+		$self->{speedmph} = $$parsedNMEA{speed};
+		$self->{timestamp} = $$parsedNMEA{"time"};
 	};
 	if ($@) {
 		return [ undef, "Waited $gpstimeout seconds, but got no usable information location from the GPS" ];
 	}
 	else {
-		printf "[1;32mLon[[1;37m%+3.6f[1;32m], Lat[[1;37m%+3.6f[1;32m], Alt[[1;37m%3.0fft[1;32m][0m\n", $$json{lon}, $$json{lat}, $$json{alt} * 3.28084 if $debug;
+		printf "[1;32mLon[[1;37m%+3.6f[1;32m], Lat[[1;37m%+3.6f[1;32m], Time[[1;37m%s[1;32m][0m\n", $self->{lon}, $self->{lat}, scalar(localtime($self->{timestamp})) if $debug;
 	}
-	
-	$self->{lon} = $$json{lon};
-	$self->{lat} = $$json{lat};
-	$self->{speedmph} = $$json{speed} * 2.23694;
-	$self->{timestamp} = $self->parseRFC3339($$json{"time"});
 
 	return [ 1 ] ;
 }
@@ -100,22 +81,23 @@ sub initialize {
 sub update {
 	my $self = shift;
 
-	my $rawtext = $self->{gpsd}->getline();
-	my $gpsjson = decode_json($rawtext);
+	my $parsedNMEA;
+	my $rawtext = $self->{devfeed}->getline();
 
-	# GPSd occasionally transmits a status update instead of a location. Fast-forward
-	# until we get a location update (if necessary).
-	until ($$gpsjson{class} eq "TPV") {
-		$rawtext = $self->{gpsd}->getline();
-		$gpsjson = decode_json($rawtext);
+	# The GPS throws lots of "sentences" for stuff like status, number
+	# of satellites in view, etc. We only care about $GPRMC lines, which
+	# are position fixes, so we may need to grab a few lines before we
+	# get what we want.
+	until ($parsedNMEA) {
+		$parsedNMEA = $self->decodeNMEA($self->{devfeed}->getline());
 	}
 
-	$self->{lon} = $$gpsjson{lon};
-	$self->{lat} = $$gpsjson{lat};
-	$self->{speedmph} = $$gpsjson{speed} * 2.23694;
-	$self->{distance} = ($self->parseRFC3339($$gpsjson{"time"}) - $self->{timestamp}) * $self->{speedmph} * 1.46667; # MPH to feet
+	$self->{lon} = $$parsedNMEA{lon};
+	$self->{lat} = $$parsedNMEA{lat};
+	$self->{speedmph} = $$parsedNMEA{speed};
+	$self->{distance} = ($$parsedNMEA{"time"} - $self->{timestamp}) * $self->{speedmph} * 1.46667; # MPH to feet
 
-	$self->{timestamp} = $self->parseRFC3339($$gpsjson{"time"});
+	$self->{timestamp} = $$parsedNMEA{"time"};
 }
 
 sub parseRFC3339 {
@@ -126,16 +108,47 @@ sub parseRFC3339 {
 	return Time::Local::timegm(0, $5, $4, $3, $2 - 1, $1) + $6;
 }
 
+sub decodeNMEA {
+	my $self = shift;
+	my $rawNMEA = shift;
+
+	unless ($rawNMEA =~ /^\$GPRMC,.*?,A,/) {
+		return undef;
+	}
+
+	chomp ($rawNMEA);
+	my $return = {};
+
+	my @nmea = split(',', $rawNMEA);
+
+	#
+	# NMEA RMC sentences: http://aprs.gids.nl/nmea/#rmc 
+	#
+	my $x = $nmea[3];
+	$x =~ s/^\d+(\d\d\.\d+)$/$1/;
+	if ($nmea[4] =~ /n/i) { $$return{lon} = ($nmea[3] - $x) + ($x / 60); }
+	else                  { $$return{lon} = 1 - (($nmea[3] - $x) + ($x / 60)); }
+
+	$x = $nmea[5];
+	$x =~ s/^\d+(\d\d\.\d+)$/$1/;
+	if ($nmea[6] =~ /w/i) { $$return{lat} = ($nmea[5] - $x) + ($x / 60); }
+	else                  { $$return{lat} = 1 - (($nmea[5] - $x) + ($x / 60)); }
+
+	($nmea[9] . $nmea[1]) =~ /(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d\.?\d*)/;
+	$$return{"time"} = Time::Local::timegm(0, $5, $4, $1, $2 - 1, $3) + $6;
+
+	$$return{speed} = $nmea[7] * 1.15078;
+
+	return $return;
+}
+
 sub shutdown {
 	my $self = shift;
 	my $debug = $self->{debug};
-	print "Cancelling GPSd WATCH..." if $debug;
-	$self->{gpsd}->print("?WATCH={\"enable\":false}\n");
-	print "[1;32mOK[0m\n" if $debug;
 
-	print "Dropping GPSd connection..." if $debug;
-	if ($self->{gpsd}->close()) { print "[1;32mOK[0m\n" if $debug; }
-	else                        { print "[1;31mFailed![0m\n" if $debug; }
+	print "Closing GPS device feed..." if $debug;
+	if ($self->{gpsfeed}->close()) { print "[1;32mOK[0m\n" if $debug; }
+	else                           { print "[1;31mFailed![0m\n" if $debug; }
 }
 
 1;
